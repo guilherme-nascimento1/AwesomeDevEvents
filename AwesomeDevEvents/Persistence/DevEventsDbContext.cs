1f2ceae44278d64f13668cfa7ca25feea5929979
Guilherme <guihsn2007@hotmail.com>
2024-03-11 18:15:23 -0300

    First Commit

---
+++ AwesomeDevEvents/Persistence/DevEventsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AwesomeDevEvents.Entities;
+
+namespace AwesomeDevEvents.Persistence {
+    public class DevEventsDbContext {
+        public List<DevEvent> DevEvents {
+            get; set;
+        }
+
+        public DevEventsDbContext() {
+            DevEvents = new List<DevEvent>();
+        }
+    }
+}
